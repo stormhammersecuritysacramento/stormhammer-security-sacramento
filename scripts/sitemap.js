453d81815ce0aa4198b9afc385ce79dd913e3069
stormhammersecuritysacramento <stormhammer.security.sacramento@gmail.com>
2026-06-08 18:19:52 -0700

    Create sitemap.js

---
+++ scripts/sitemap.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const pages = require("../pages.json");
+
+let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+pages.forEach(p => {
+  xml += `
+<url>
+<loc>https://YOURUSERNAME.github.io/stormhammer-security-sacramento/${p}</loc>
+</url>`;
+});
+
+xml += `</urlset>`;
+
+fs.writeFileSync("sitemap.xml", xml);
+
+console.log("Sitemap generated");
